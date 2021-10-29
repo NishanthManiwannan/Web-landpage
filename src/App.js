bdc35046b56914984987c53715b6572dc56ce642
Nishanth <m.nishanth1397@gmail.com>
2021-10-29 16:28:16 +0530

    Navigation bar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+
+        <Switch>
+          <Route path="/" exact></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
